b6eba7f78108464b1498507b39e9c97f9525ba44
unknown <monmaramba@yahoo.com>
2019-06-23 08:25:09 -0700

    clean-up

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-const choices = document.querySelectorAll('.choice');
-const score = document.getElementById('score');
-const result = document.getElementById('result');
-const restart = document.getElementById('restart');
-const modal = document.querySelector('.modal');
-const scoreboard = {
-    player: 0,
-    computer: 0
-};
+const choices = document.querySelectorAll('.choice'),
+    score = document.getElementById('score'),
+    result = document.getElementById('result'),
+    restart = document.getElementById('restart'),
+    modal = document.querySelector('.modal'),
+    scoreboard = {
+        player: 0,
+        computer: 0
+    };
 
 // Play game
 const play = e => {
     restart.style.display = 'inline-block';
-    const playerChoice = e.target.id;
-    const computerChoice = getComputerChoice();
-    const winner = getWinner(playerChoice, computerChoice);
+    const playerChoice = e.target.id,
+          computerChoice = getComputerChoice(),
+          winner = getWinner(playerChoice, computerChoice);
     showWinner(winner, computerChoice);
 }
 
 // get computer choice
 const getComputerChoice = () => {
     const rand = Math.random();
-    if(rand < 0.34) {
+    if (rand < 0.34) {
         return 'rock'
-    } else if(rand <= .67) {
+    } else if (rand <= .67) {
         return 'paper'
     } else {
         return 'scissors';
@@ -30,32 +30,32 @@ const getComputerChoice = () => {
 }
 
 function getWinner(playerChoice, computerChoice) {
-    if(playerChoice ===  computerChoice) {
+    if (playerChoice === computerChoice) {
         return 'draw';
-    } else if(playerChoice === 'rock' && computerChoice === 'paper') {
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
         return 'Computer';
-    } else if(playerChoice === 'rock' && computerChoice === 'scissors') {
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
         return 'Player';
-    } else if(playerChoice === 'paper' && computerChoice === 'rock') {
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
         return 'Player';
-    } else if(playerChoice === 'paper' && computerChoice === 'scissors') {
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
         return 'Computer';
-    } else if(playerChoice === 'scissors' && computerChoice === 'paper') {
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
         return 'Player';
-    } else if(playerChoice === 'scissors' && computerChoice === 'rock') {
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
         return 'Computer';
-    } 
+    }
 }
 
 const showWinner = (winner, computerChoice) => {
-    if(winner === 'Player') {
+    if (winner === 'Player') {
         scoreboard.player++;
         result.innerHTML = `
             <h1 class="text-win">You win!<h1>
             <i class="fas fa-hand-${computerChoice}" fa-10x></i>
             <p>Computer chose ${computerChoice}</p>
         `;
-    } else if(winner === 'Computer') {
+    } else if (winner === 'Computer') {
         scoreboard.computer++;
         result.innerHTML = `
             <h1 class="text-lose">You lose!<h1>
@@ -79,7 +79,7 @@ const showWinner = (winner, computerChoice) => {
 
 // To clear the modal
 const clearModal = e => {
-    if(e.target == modal){
+    if (e.target == modal) {
         modal.style.display = 'none';
     }
 }
